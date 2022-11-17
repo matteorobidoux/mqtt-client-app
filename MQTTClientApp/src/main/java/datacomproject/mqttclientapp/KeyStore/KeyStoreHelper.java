5a0f603753714e0b0eaf0c5fe3a648e1784f037f
RimDallali <dallali.rim1@gmail.com>
2022-11-16 20:02:06 -0500

    added application running for keystore

--- MQTTClientApp/src/main/java/datacomproject/mqttclientapp/KeyStore/KeyStoreHelper.java
+++ MQTTClientApp/src/main/java/datacomproject/mqttclientapp/KeyStore/KeyStoreHelper.java
@@ -1,59 +1,16 @@
 package datacomproject.mqttclientapp.KeyStore;
 
-import java.util.Scanner;
-import java.io.Console;
-import java.io.EOFException;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.security.cert.Certificate;
 import java.security.*;
-import java.text.Normalizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Rim Dallali
  */
 public class KeyStoreHelper {
 
-    private String filepath;
     private char[] password;
     private KeyStore ks;
-
-    public void getUserInput() throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        Console console = System.console();
-
-        // getting and validating filename
-        System.out.println("------------------------------------------------");
-        System.out.println("-- Provide path where the keystore is located --");
-        boolean validKeyStore = false;
-        while (!validKeyStore) {
-            try {
-                filepath = getFilename();
-                validKeyStore = true;
-                if (validKeyStore) {
-                    // getting and validating password
-                    System.out.println("-------- Provide password --------");
-                    password = console.readPassword();
-                    while (!validatePassword(password)) {
-                        System.out.println("--- Invalid password, try again --");
-                        password = console.readPassword();
-                    }
-                    // load the keystore
-                    this.ks = loadKeyStore(password, filepath);
-                }
-            } catch (EOFException e) {
-                System.out.println("Given file path is INVALID");
-                validKeyStore = false;
-            } catch (IOException ie) {
-                System.out.println("Wrong KeyStore password OR Invalid KeyStore path, try again");
-                validKeyStore = false;
-            }
-        }
-        scanner.close();
-    }
 
     /**
      * Load keyStore from filename and password user input
@@ -64,14 +21,15 @@ public class KeyStoreHelper {
      * @throws Exception
      */
     public KeyStore loadKeyStore(char[] password, String filename) throws Exception {
+        this.password = password;
         System.out.println("loading the key store");
         System.out.println("---------------------");
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        this.ks = KeyStore.getInstance(KeyStore.getDefaultType());
         FileInputStream fis = new FileInputStream(filename);
         ks.load(fis, password);
         System.out.println("KeyStore has been loaded");
         return ks;
-    }
+    } 
 
     /**
      * Get trusted certificate from keyStore at the given alias
@@ -81,33 +39,31 @@ public class KeyStoreHelper {
      * @throws KeyStoreException
      */
     public Certificate extractCertificate(String alias) throws KeyStoreException {
-        System.out.println("Get Trusted Certificate");
-        System.out.println("-----------------------");
-
+        System.out.println("------Getting the Certificate-------");
         Certificate trustedCertificate = ks.getCertificate(alias);
-        return trustedCertificate;
+        System.out.println("Trusted Certificate has been retrieved");
+        return trustedCertificate; 
     }
 
     /**
      * Gets the private key from given certificate at the specified alias
      * 
      * @param password
-     * @param alias 
+     * @param alias
      * @return PrivateKey
-     * @throws Exception
+     * @throws Exception 
      */
-    public PrivateKey extractPrivateKey(String alias, char[] password) throws Exception {
+    public PrivateKey extractPrivateKey(String alias) throws Exception {
         System.out.println("------Getting the Private Key-------");
-        System.out.println("------------------------------------");
         KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(password);
+        KeyStore.PrivateKeyEntry privateKeyEntry = null; 
 
-        KeyStore.PrivateKeyEntry privateKeyEntry = null;
-
-        // get private key from keystore 
-        privateKeyEntry = (KeyStore.PrivateKeyEntry) ks.getEntry(alias, protParam); 
+        // get private key from keystore
+        privateKeyEntry = (KeyStore.PrivateKeyEntry) ks.getEntry(alias, protParam);
         PrivateKey privateKey = privateKeyEntry.getPrivateKey();
+        System.out.println("Private key has been retrieved");
         return privateKey;
-    } 
+    }
 
     /**
      * Store the input certificate to the keystore at input alias
@@ -117,90 +73,30 @@ public class KeyStoreHelper {
      * @param certificate
      * @return boolean
      */
-    public boolean storeCertificate(String alias, Certificate certificate) {
-        System.out.println("Storing the certificate");
-        System.out.println("-----------------------");
+    public boolean storeCertificate(Certificate certificate, String alias) {
+        System.out.println("Storing the certificate"); 
         try {
             ks.setCertificateEntry(alias, certificate);
             System.out.println("certificate alias entry has been set\nalias: " + alias + "\ncertificate: "
-                + certificate.getPublicKey().toString());
+                    + certificate.getPublicKey().toString());
             return true;
         } catch (KeyStoreException e) {
             return false;
-        } 
+        }
     }
 
-    /**
+     /**
      * Checks if the given alias is part of the aliases in the keystore
      * 
-     * @param alias
+     * @param alias 
      * @return boolean
      * @throws KeyStoreException
      */
     public boolean checkAlias(String alias) throws KeyStoreException {
-        while (ks.aliases().hasMoreElements()) {
-            if (ks.aliases().nextElement().equals(alias)) {
-                return true;
-            }
+        boolean validAlias = ks.containsAlias(alias);
+        if(!validAlias) {
+            throw new IllegalArgumentException("Alias given is invalid");
         }
-        return false;
-    }
-
-    /**
-     * Prompts the user to enter a filename for where to store the secret key
-     * Helper method
-     * 
-     * @return String
-     * @throws EOFException
-     */
-    private String getFilename() throws EOFException {
-        Scanner scanner = new Scanner(System.in);
-        boolean validFile = false;
-        String normalizedFilename = null;
-        // getting and validating filename
-        while (!validFile) {
-            System.out.println("--- Provide filename ---");
-            String filename = scanner.nextLine();
-
-            // normalize the filename input
-            normalizedFilename = Normalizer.normalize(filename, Normalizer.Form.NFKC);
-
-            // check the filename only contains allowed characters
-            Pattern filePattern = Pattern.compile("^[\\\\/.-_:a-zA-Z0-9]{1,255}$");
-            Matcher matcher = filePattern.matcher(normalizedFilename);
-
-            // checks if the file actually exists
-            File file = new File(normalizedFilename);
-            if (file.exists() && file.isFile() && matcher.find()) {
-                validFile = true;
-            } else if (!file.exists() || file.isFile()) {
-                if (file.getParentFile() != null) {
-                    file.getParentFile().mkdirs();
-                }
-                validFile = matcher.find();
-            }
-        }
-        return normalizedFilename;
-    }
-
-    /**
-     * Password validation
-     * Helper method
-     * 
-     * @param password
-     * @return boolean
-     */
-    private boolean validatePassword(char[] password) {
-        return (password.length >= 6 && password.length <= 30);
-    }
-
-    /**
-     * @param keyStore
-     * 
-     * TEST HELPER
-     * sets the keystore class instance for the unit tests
-     */
-    public void setKeyStore(KeyStore keyStore) {
-        ks = keyStore;
+        return true;
     }
 }
--- MQTTClientApp/src/test/java/datacomproject/mqttclientapp/mqtt/KeyStoreHelperTest.java
+++ MQTTClientApp/src/test/java/datacomproject/mqttclientapp/mqtt/KeyStoreHelperTest.java
@@ -26,15 +26,13 @@ public class KeyStoreHelperTest {
     KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
     ECGenParameterSpec ecParaSpec = new ECGenParameterSpec("secp256r1");
     generator.initialize(ecParaSpec);
-
     ksh = new KeyStoreHelper();
-    ks = ksh.loadKeyStore(password, filename);
-    ksh.setKeyStore(ks);
   }
 
   @Test
   public void testExtractCertificate() throws Exception {
     String alias = "TEST";
+    ksh.loadKeyStore(password, filename);
     Certificate certificate = ksh.extractCertificate(alias);
     assertNotNull(certificate);
   }
@@ -42,11 +40,12 @@ public class KeyStoreHelperTest {
   @Test
   public void testStoreCertificate() throws Exception {
     String alias = "STORETEST";
+    ksh.loadKeyStore(password, filename);
     Certificate originalCertificate = ksh.extractCertificate("TEST");
     assertNotNull(originalCertificate);
 
     // store the certificate to the new alias STORETEST
-    ksh.storeCertificate(alias, originalCertificate);
+    ksh.storeCertificate(originalCertificate, alias);
 
     // get the certificate from the new alias we stored it to
     Certificate storedCertificate = ksh.extractCertificate(alias);
@@ -55,19 +54,21 @@ public class KeyStoreHelperTest {
 
   @Test
   public void testLoadKeyStore() throws Exception {
+    KeyStore ks = ksh.loadKeyStore(password, filename);
     assertNotNull(ks);
   }
 
   @Test
   public void testGetKeyStoreInfo() throws Exception {
+    ksh.loadKeyStore(password, filename);
     Certificate certificate = ksh.extractCertificate("TEST");
     assertNotNull(certificate);
   }
 
   @Test
   public void testExtractPrivateKey() throws Exception {
-    // Certificate certificate = ksh.extractCertificate("TEST");
-    PrivateKey pk = ksh.extractPrivateKey("TEST", password);
+    ksh.loadKeyStore(password, filename);
+    PrivateKey pk = ksh.extractPrivateKey("TEST");
     assertNotNull(pk);
   }
 }
