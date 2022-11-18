1e425785d35bab78fb3afaa07fa2b3b20bbe7254
Matteo Robidoux <SDJ00008@gmail.com>
2022-11-18 14:21:09 -0500

    Assuring all is pushed including file folder chnaging

--- MQTTClientApp/src/test/java/datacomproject/mqtt/MQTTTest.java
+++ MQTTClientApp/src/test/java/datacomproject/mqtt/MQTTTest.java
@@ -1,4 +1,4 @@
-package datacomproject.mqttclientapp;
+package datacomproject.mqtt;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
