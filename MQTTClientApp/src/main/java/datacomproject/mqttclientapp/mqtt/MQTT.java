a88e58d42e9b9490398b24f1f369c38b5365a406
RimDallali <dallali.rim1@gmail.com>
2022-12-02 18:08:22 -0500

    add documentation

--- MQTTClientApp/src/main/java/datacomproject/mqttclientapp/mqtt/MQTT.java
+++ MQTTClientApp/src/main/java/datacomproject/mqttclientapp/mqtt/MQTT.java
@@ -22,189 +22,260 @@ import static com.hivemq.client.mqtt.MqttGlobalPublishFilter.ALL;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
  * @author Matteo
  */
 public class MQTT {
-    
-    private Mqtt5BlockingClient client;
-    private String username;
-    private SignatureHelper signatureHelper = new SignatureHelper();
-    public List<JSONObject> jsonObjectsMatteo = new ArrayList<JSONObject>();
-    public List<JSONObject> jsonObjectsRim = new ArrayList<JSONObject>();
-    public List<JSONObject> certificates = new ArrayList<JSONObject>();
-    public FXScreen fxScreen;
-    public KeyStoreHelper ksh;
 
-    // Retrives MQTT Client
-    public Mqtt5BlockingClient getMqttClient(){
-        try{
-            System.out.println("Retrieving MQTT Client...");
-            client = MqttClient.builder()
-                .useMqttVersion5()
-                .serverHost("5a81e32977cf48798ae1059437f7dd15.s1.eu.hivemq.cloud")
-                .serverPort(8883)
-                .sslWithDefaultConfig()
-                .buildBlocking();
-        } catch(Mqtt5ConnAckException e){
-            System.out.println("Unable to Retrieve MQTT Client!");
-            System.exit(0);
-        }
-        System.out.println("Retrieved MQTT Client!");   
-        return client;
-    }
-    
-    // Connects to HiveMQ using client credentials
-    public boolean createConnection(String username, String password){
-        System.out.println("Connecting to HiveMQ...");
-        try{
-            client.connectWith()
-                .simpleAuth()
-                .username(username)
-                .password(UTF_8.encode(password))
-                .applySimpleAuth()
-                .send();
-            System.out.println("Connected successfully");
-            this.username = username;
-            return true;
-        } catch(Mqtt5ConnAckException | NullPointerException e){
-            System.out.println("Invalid Username or Password, Try Again...");
-            return false;
-        }
-    }
-    
-    // Client subscribes to all topics begining with mqtt/
-    public void subscribe(){
-        client.subscribeWith()
-            .topicFilter("mqtt/#")
-            .send();
-    }
-    
-    // Rerieves all messages sent to the client and/or certificate, verifies signature if it is a message and adds the data to the correct user List
-    public void retrieveMessage() {
-        client.toAsync().publishes(ALL, publish -> { 
-            JSONObject jsonObject = new JSONObject(UTF_8.decode(publish.getPayload().get()).toString());
-            if(!jsonObject.has("certificate")){    
-                try {
-                    byte[] signature = Base64.getDecoder().decode(jsonObject.get("signature").toString());
-                    jsonObject.remove("signature");
-                    Certificate certificate = ksh.extractCertificate(publish.getTopic().toString().split("/")[1]);
-                    
-                    if(signatureHelper.verifySignature(signature, certificate.getPublicKey(), "SHA256withECDSA", jsonObject.toString())){
-                        System.out.println("Received message: " + publish.getTopic() + " -> " + jsonObject);
-                        if(publish.getTopic().toString().contains("matteorobidoux")){
-                           if(fxScreen.row2.username.contains("matteorobidoux")){
-                                if(publish.getTopic().toString().contains("dht")){
-                                    fxScreen.row2.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"), jsonObject.getString("timestamp"));
-                                } else if(publish.getTopic().toString().contains("image")){
-                                    fxScreen.row2.updateImage(imageToInputStream(jsonObject.get("image")));
-                                    System.out.println("image");
-                                } else if(publish.getTopic().toString().contains("doorbell")){
-                                    fxScreen.row2.updateDoorbell(jsonObject.getString("doorbell"));
-                                }
-                            }
-                        } if(publish.getTopic().toString().contains("rimdallali")){
-                            if(fxScreen.row1.username.contains("rimdallali")){
-                                if(publish.getTopic().toString().contains("dht")){
-                                    fxScreen.row1.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"), jsonObject.getString("timestamp"));
-                                } else if(publish.getTopic().toString().contains("image")){
-                                    fxScreen.row1.updateImage(imageToInputStream(jsonObject.get("image")));
-                                } else if(publish.getTopic().toString().contains("doorbell")){
-                                    fxScreen.row1.updateDoorbell(jsonObject.getString("doorbell"));
-                                }
-                            }
-                        } if(publish.getTopic().toString().contains("carletondavis")){
-                            if(fxScreen.row3.username.contains("carletondavis")){
-                                if(jsonObject.has("dht")){
-                                    fxScreen.row3.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"), jsonObject.getString("timestamp"));
-                                } else if(publish.getTopic().toString().contains("image")){
-                                    fxScreen.row3.updateImage(imageToInputStream(jsonObject.get("image")));
-                                } else if(jsonObject.has("doorbell")){
-                                    fxScreen.row3.updateDoorbell(jsonObject.getString("doorbell"));
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    System.out.println("There Was A Problem Verifying The Signature..." + e);
-                }
-            } else {
-                System.out.println("Received Certificate: " + publish.getTopic() + " -> " + jsonObject);
-                try {
-                    CertificateFactory cf   = CertificateFactory.getInstance("X.509");
-                    Certificate c =  cf.generateCertificate(new ByteArrayInputStream(Base64.getDecoder().decode(jsonObject.get("certificate").toString())));
-                    ksh.storeCertificate(c, publish.getTopic().toString().split("/")[1]);
-      
-                } catch (JSONException e) {
-                    System.out.println("Error Retrieving Certificate");
-                } catch (CertificateException ex) {
-                    Logger.getLogger(MQTT.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-    }
-    
-    // Publish message to a specific topic sending a JSON object which contains the datas being sent including the signature
-    public JSONObject publishDataMessage(PrivateKey privateKey, String topic, JSONObject data) throws Exception{
-        byte[] signature = signatureHelper.signMessage(privateKey, data.toString());
-        data.put("signature", new String(Base64.getEncoder().encode(signature), "UTF-8")); 
-        client.publishWith()
-                .topic(getTopic(topic))
-                .payload(UTF_8.encode(data.toString()))
-                .retain(true)
-                .send();
-        return data;
-    }
+	private Mqtt5BlockingClient client;
+	private String username;
+	private String host = "5a81e32977cf48798ae1059437f7dd15.s1.eu.hivemq.cloud";
+	private SignatureHelper signatureHelper = new SignatureHelper();
+	public List<JSONObject> jsonObjectsMatteo = new ArrayList<JSONObject>();
+	public List<JSONObject> jsonObjectsRim = new ArrayList<JSONObject>();
+	public List<JSONObject> certificates = new ArrayList<JSONObject>();
+	public FXScreen fxScreen;
+	public KeyStoreHelper ksh;
 
-    // Publishes a message with the certificate within it
-    public JSONObject publishCertificateMessage(Certificate certificate) throws CertificateEncodingException, JSONException, UnsupportedEncodingException{
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("certificate", new String(Base64.getEncoder().encode(certificate.getEncoded()), "UTF-8"));
-        client.publishWith()
-                .topic(getTopic("certificate"))
-                .payload(UTF_8.encode(jsonObject.toString()))
-                .retain(true)
-                .send();
-        return jsonObject;
-    }
+	private String user1 = "rimdallali";
+	private String user2 = "matteorobidoux";
+	private String user3 = "carletondavis";
 
-    // Gets the topic using the topic path and client username to create it
-    private String getTopic(String topicPath){
-        return "mqtt/" + username + "/" + topicPath;
-    }
+	/**
+	 * Retrieve MQTT client
+	 * 
+	 * @return Mqtt5BlockingClient
+	 */
+	public Mqtt5BlockingClient getMqttClient() {
+		try {
+			System.out.println("Retrieving MQTT Client...");
+			client = MqttClient.builder()
+					.useMqttVersion5()
+					.serverHost(host)
+					.serverPort(8883)
+					.sslWithDefaultConfig()
+					.buildBlocking();
+		} catch (Mqtt5ConnAckException e) {
+			System.out.println("Unable to Retrieve MQTT Client!");
+			System.exit(0);
+		}
+		System.out.println("Retrieved MQTT Client!");
+		return client;
+	}
 
-    // Disconnects from the client
-    public void disconnect(){
-        client.disconnect();
-    }
+	/**
+	 * Connect to HiveMQ using the user credentials
+	 * 
+	 * @param username
+	 * @param password
+	 * @return boolean
+	 */
+	public boolean createConnection(String username, String password) {
+		System.out.println("Connecting to HiveMQ...");
+		try {
+			client.connectWith()
+					.simpleAuth()
+					.username(username)
+					.password(UTF_8.encode(password))
+					.applySimpleAuth()
+					.send();
+			System.out.println("Connected successfully");
+			this.username = username;
+			return true;
+		} catch (Mqtt5ConnAckException | NullPointerException e) {
+			System.out.println("Invalid Username or Password, Try Again...");
+			return false;
+		}
+	}
 
-    // Takes in a jsonObject and returns a certificate
-    public Certificate retrieveCertificate(JSONObject jsonCertificate, String key) throws CertificateException{
-        if(jsonCertificate.has(key)){
-            CertificateFactory cf   = CertificateFactory.getInstance("X.509");
-            return cf.generateCertificate(new ByteArrayInputStream(Base64.getDecoder().decode(jsonCertificate.get(key).toString())));
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * Subscribe to all topics beginning with mqtt/
+	 */
+	public void subscribe() {
+		client.subscribeWith()
+				.topicFilter("mqtt/#")
+				.send();
+	}
 
-    public void setVariables(FXScreen setFxScreen, KeyStoreHelper ksh1){
-        fxScreen = setFxScreen;
-        ksh = ksh1;
-    }
+	/**
+	 * Retrieve all messages sent to the client and/or certificate
+	 * Verify signature if it is a message and adds the data to the correct user
+	 * list
+	 */
+	public void retrieveMessage() {
+		client.toAsync().publishes(ALL, publish -> {
+			JSONObject jsonObject = new JSONObject(UTF_8.decode(publish.getPayload().get()).toString());
+			if (!jsonObject.has("certificate")) {
+				try {
+					byte[] signature = Base64.getDecoder().decode(jsonObject.get("signature").toString());
+					jsonObject.remove("signature");
+					Certificate certificate = ksh.extractCertificate(publish.getTopic().toString().split("/")[1]);
 
-    public InputStream imageToInputStream(Object image) throws IOException{
-        byte[] img = Base64.getDecoder().decode(image.toString());
-        return new ByteArrayInputStream(img);
-    }
+					if (signatureHelper.verifySignature(signature, certificate.getPublicKey(), "SHA256withECDSA",
+							jsonObject.toString())) {
+						System.out.println("Received message: " + publish.getTopic() + " -> " + jsonObject);
+						if (publish.getTopic().toString().contains(user2)) {
+							if (fxScreen.row2.username.contains(user2)) {
+								if (publish.getTopic().toString().contains("dht")) {
+									fxScreen.row2.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"),
+											jsonObject.getString("timestamp"));
+								} else if (publish.getTopic().toString().contains("image")) {
+									fxScreen.row2.updateImage(imageToInputStream(jsonObject.get("image")));
+									System.out.println("image");
+								} else if (publish.getTopic().toString().contains("doorbell")) {
+									fxScreen.row2.updateDoorbell(jsonObject.getString("doorbell"));
+								}
+							}
+						}
+						if (publish.getTopic().toString().contains(user1)) {
+							if (fxScreen.row1.username.contains(user1)) {
+								if (publish.getTopic().toString().contains("dht")) {
+									fxScreen.row1.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"),
+											jsonObject.getString("timestamp"));
+								} else if (publish.getTopic().toString().contains("image")) {
+									fxScreen.row1.updateImage(imageToInputStream(jsonObject.get("image")));
+								} else if (publish.getTopic().toString().contains("doorbell")) {
+									fxScreen.row1.updateDoorbell(jsonObject.getString("doorbell"));
+								}
+							}
+						}
+						if (publish.getTopic().toString().contains(user3)) {
+							if (fxScreen.row3.username.contains(user3)) {
+								if (jsonObject.has("dht")) {
+									fxScreen.row3.updateDHT(jsonObject.getDouble("temperature"), jsonObject.getDouble("humidity"),
+											jsonObject.getString("timestamp"));
+								} else if (publish.getTopic().toString().contains("image")) {
+									fxScreen.row3.updateImage(imageToInputStream(jsonObject.get("image")));
+								} else if (jsonObject.has("doorbell")) {
+									fxScreen.row3.updateDoorbell(jsonObject.getString("doorbell"));
+								}
+							}
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("There Was A Problem Verifying The Signature..." + e);
+				}
+			} else {
+				System.out.println("Received Certificate: " + publish.getTopic() + " -> " + jsonObject);
+				try {
+					CertificateFactory cf = CertificateFactory.getInstance("X.509");
+					Certificate c = cf.generateCertificate(
+							new ByteArrayInputStream(Base64.getDecoder().decode(jsonObject.get("certificate").toString())));
+					ksh.storeCertificate(c, publish.getTopic().toString().split("/")[1]);
+
+				} catch (JSONException e) {
+					System.out.println("Error Retrieving Certificate");
+				} catch (CertificateException ex) {
+					Logger.getLogger(MQTT.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Publish message to a specific topic sending a JSON object which contains the
+	 * data being sent including the signature.
+	 * 
+	 * @param privateKey
+	 * @param topic
+	 * @param data
+	 * @return JSONObject
+	 * 
+	 * @throws Exception
+	 */
+	public JSONObject publishDataMessage(PrivateKey privateKey, String topic, JSONObject data) throws Exception {
+		byte[] signature = signatureHelper.signMessage(privateKey, data.toString());
+		data.put("signature", new String(Base64.getEncoder().encode(signature), "UTF-8"));
+		client.publishWith()
+				.topic(getTopic(topic))
+				.payload(UTF_8.encode(data.toString()))
+				.retain(true)
+				.send();
+		return data;
+	}
+
+	/**
+	 * Publishe a message with the certificate within it
+	 * 
+	 * @param certificate
+	 * @return JSONObject
+	 * 
+	 * @throws Exception
+	 */
+	public JSONObject publishCertificateMessage(Certificate certificate)
+			throws CertificateEncodingException, JSONException, UnsupportedEncodingException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("certificate", new String(Base64.getEncoder().encode(certificate.getEncoded()), "UTF-8"));
+		client.publishWith()
+				.topic(getTopic("certificate"))
+				.payload(UTF_8.encode(jsonObject.toString()))
+				.retain(true)
+				.send();
+		return jsonObject;
+	}
+
+	/**
+	 * Get the topic using the topic path and client username to create it
+	 * 
+	 * @param topicPath
+	 * @return String
+	 */
+	private String getTopic(String topicPath) {
+		return "mqtt/" + username + "/" + topicPath;
+	}
+
+	/**
+	 * Disconnect from the client
+	 */
+	public void disconnect() {
+		client.disconnect();
+	}
+
+	/**
+	 * Return certificate corresponding the given jsonObject and key
+	 * 
+	 * @param jsonCertificate
+	 * @param key
+	 * @return Certificate
+	 * 
+	 * @throws CertificateException
+	 */
+	public Certificate retrieveCertificate(JSONObject jsonCertificate, String key) throws CertificateException {
+		if (jsonCertificate.has(key)) {
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			return cf.generateCertificate(
+					new ByteArrayInputStream(Base64.getDecoder().decode(jsonCertificate.get(key).toString())));
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Set the fxscreen and keystore helper objects
+	 * 
+	 * @param setFxScreen
+	 * @param ksh1
+	 */
+	public void setVariables(FXScreen setFxScreen, KeyStoreHelper ksh1) {
+		fxScreen = setFxScreen;
+		ksh = ksh1;
+	}
+
+	/**
+	 * Convert image to inputStream
+	 * 
+	 * @param image
+	 * @return InputStream
+	 */
+	public InputStream imageToInputStream(Object image) throws IOException {
+		byte[] img = Base64.getDecoder().decode(image.toString());
+		return new ByteArrayInputStream(img);
+	}
 }
